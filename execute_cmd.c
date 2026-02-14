1e42ae9b5763839c8b20847b180c3eb93a1a3ff8
Natiq7855 <natabbasli@gmail.com>
2026-02-14 08:19:47 +0000

    SDF

--- execute_cmd.c
+++ execute_cmd.c
@@ -1,40 +1,45 @@
 #include "shell.h"
 
 /**
- * execute_cmd - Tokenizes the string and executes the command with args.
- * @line: The raw input string from getline.
+ * execute_cmd - Tokenizes a line into arguments and executes it.
+ * @line: The raw string input from the user.
+ *
+ * Return: void.
  */
 void execute_cmd(char *line)
 {
-	char *args[64];
+	char *args[1024];
 	char *token;
 	int i = 0;
 	pid_t child_pid;
 	int status;
 
-	token = strtok(line, " \t\r\n");
-	while (token != NULL && i < 63)
+	token = strtok(line, " \t\r\n\a");
+	while (token != NULL)
 	{
 		args[i] = token;
 		i++;
-		token = strtok(NULL, " \t\r\n");
+		token = strtok(NULL, " \t\r\n\a");
 	}
 	args[i] = NULL;
-
 	if (args[0] == NULL)
 		return;
-
 	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("fork");
+		return;
+	}
 	if (child_pid == 0)
 	{
 		if (execve(args[0], args, environ) == -1)
 		{
-			perror("./shell");
+			perror("./hsh");
 			exit(EXIT_FAILURE);
 		}
 	}
-	else if (child_pid < 0)
-		perror("Fork Error");
 	else
+	{
 		wait(&status);
+	}
 }
